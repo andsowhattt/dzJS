62c174936f7ae9b48d2518d2681a4751b2c6ba1e
Alina Miedviedieva <a.ndsowhattt@gmail.com>
2023-02-09 09:32:07 +0200

    2 branch 1 commit

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
 //1
-const userNames= ["Петрик Ольга Іванівна", "Гнатюк Петро Антонович", "Рудко Андрій Опанасович"];
-let initials = userNames.sort().map((userName) => {
-	return userName.split(' ').map((userName) => userName.charAt(0)).join('.');
-})
 
-console.log(initials);
+function durationBetweenDates(firstDate, secondDate, dimensionBetween) {
+	const calcFirstDate = new Date(firstDate);
+	const calcSecondDate = new Date(secondDate);
+	const millisecondsDiff = calcSecondDate.getTime() - calcFirstDate.getTime();
+	if (dimensionBetween === 'seconds'){
+	  const secondsType = millisecondsDiff / 1000;
+	  return secondsType + ' seconds';
+	}
+	if (dimensionBetween === 'minutes'){
+	  const minutesType = Math.ceil(millisecondsDiff / (1000 * 60));
+	  return minutesType + ' minutes';
+	}
+	if (dimensionBetween === 'hours'){
+	  const hoursType = millisecondsDiff / (1000 * 3600);
+	  return hoursType + ' hours';
+	}
+	if (dimensionBetween === 'days'){
+	  const daysType = Math.abs(millisecondsDiff / (1000 * 3600 * 24));
+	  return daysType + ' days';
+	} else {
+	  return console.log('Not Found');
+	}
+  }
+  console.log(durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds'));
+  console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days'));
 
 //2
 
-const currentMaxValue = 4589;
-let reverseMaxValue = Number([...currentMaxValue.toString()].reverse().join(""));
-
-console.log(reverseMaxValue); 
-console.log(typeof reverseMaxValue);
+const priceData = {
+	Apples: '23.4',
+	BANANAS: '48',
+	oRAngGEs: '48.7584',
+}
+const optimizer = data =>
+	Object.keys(data).reduce((acc, key) => {
+		acc[key.toLowerCase()] = data[key] = Number(data[key]).toFixed(2);
+		return acc;
+	}, {});
+const updatedPriceData = optimizer(priceData);
+console.log(updatedPriceData);
 
 //3
 
-const resultsArray = [1, 2, [3, [4]]];
-let productOfArray = resultsArray.flat(Infinity).reduce((acc, cur) => {
-	return acc+cur;
-}, 14);
+function recursiveOddSumTo(number) {
+	if (number <= 1)
+		return 1;
+	else
+		if (number % 2 == 0)
+			number--;
+	return number + recursiveOddSumTo(number - 2);
+}
+console.log(recursiveOddSumTo(10))
 
-console.log(productOfArray);
+function iterativeOddSumTo(number) {
+	let sum = 0;
+	for (let i = 1; i <= number; i++) {
+		if (i % 2 !== 0) {
+			sum = sum + i;
+		}
+	}
+	return sum;
+}
+console.log(iterativeOddSumTo(10));
